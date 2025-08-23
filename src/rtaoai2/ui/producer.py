3e6d0a2446c54993d879ac213807f73d47378193
Damien BIHEL <biheldamien@gmail.com>
2025-08-24 00:36:52 +0200

    refactor(server): use official openai client

--- src/rtaoai2/ui/producer.py
+++ src/rtaoai2/ui/producer.py
@@ -14,17 +14,14 @@ class EventProducer:
     def __init__(self, websocket):
         self.websocket = websocket
 
-    async def on_response_audio_delta_event(self, event):
-        await self.websocket.send_json(make_audio(event.delta))
+    async def on_response_audio_delta_event(self, delta: str):
+        await self.websocket.send_json(make_audio(delta))
 
-    async def on_response_audio_transcript_delta_event(self, delta):
+    async def on_response_audio_transcript_delta_event(self, delta: str):
         await self.websocket.send_json(make_audio_transcript(delta))
 
-    async def on_response_audio_input_transcript_done_event(self, transcript):
+    async def on_response_audio_input_transcript_done_event(self, transcript: str):
         await self.websocket.send_json(make_input_transcript(transcript))
 
-    async def on_response_created(self, event):
-        pass
-
-    async def on_response_done(self, event):
+    async def on_response_done(self):
         await self.websocket.send_json({"type": "message", "data": "response.done"})
